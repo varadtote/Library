2e821c5dc79caf3fa87194bca19e935d9be55a9c
Varad Tote <varadtote1111@gmail.com>
2023-02-13 11:45:54 +0530

    Modal Opening and closing added

--- main.js
+++ main.js
@@ -13,3 +13,14 @@ const Book1 = new BookInfo("The Hobbit", "JRR", 59, "not read")
 console.log(Book1.info())
 
 
+// popup modal open
+
+function ModalOpen() {
+    // console.log("Clicked")
+    document.querySelector(".modal").style.display = 'revert';
+}
+function ModalClose() {
+    // console.log("Clicked")
+    document.querySelector(".modal").style.display = 'none';
+}
+
