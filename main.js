68811a15f2862b37e0bfd90d908650ef9ee9762a
Varad Tote <varadtote1111@gmail.com>
2023-02-13 09:38:19 +0530

    styles added

---
+++ main.js
@@ -0,0 +1,13 @@
+function BookInfo(BookName, Author, Totalpages, status) {
+    this.BookName = BookName
+    this.Author = Author
+    this.Totalpages = Totalpages
+    this.status = status
+
+    this.info = function () {
+        return `${this.BookName} by ${this.Author}, ${this.Totalpages} pages, ${this.status}`
+    }
+}
+
+const Book1 = new BookInfo("The Hobbit", "JRR", 59, "not read")
+console.log(Book1.info())
