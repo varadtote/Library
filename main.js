522e7c63b0be461862567fdd5ff78032ee6d67db
Varad Tote <varadtote1111@gmail.com>
2023-02-13 21:02:34 +0530

    stuck at getting form info

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
+/* eslint-disable no-unused-vars */
+
 function BookInfo(BookName, Author, Totalpages, status) {
-    this.BookName = BookName
-    this.Author = Author
-    this.Totalpages = Totalpages
-    this.status = status
+    this.BookName = BookName;
+    this.Author = Author;
+    this.Totalpages = Totalpages;
+    this.status = status;
 
     this.info = function () {
-        return `${this.BookName} by ${this.Author}, ${this.Totalpages} pages, ${this.status}`
-    }
+        return `${this.BookName} by ${this.Author}, ${this.Totalpages} pages, ${this.status}`;
+    };
 }
 
-const Book1 = new BookInfo("The Hobbit", "JRR", 59, "not read")
-console.log(Book1.info())
-
+const Book1 = new BookInfo("The Hobbit", "JRR", 59, "not read");
+console.log(Book1.info());
 
 // popup modal open
 
 function ModalOpen() {
-    // console.log("Clicked")
-    document.querySelector(".modal").style.display = 'revert';
+    document.querySelector(".modal").style.display = "revert";
 }
 function ModalClose() {
-    // console.log("Clicked")
-    document.querySelector(".modal").style.display = 'none';
+    document.querySelector(".modal").style.display = "none";
 }
 
+// Collect Form input
+
+let form = document.getElementById("AddBookForm");
+let BookTitle = document.querySelector("#bookTitle");
+let Author = document.querySelector("#author");
+let TotalPages = document.querySelector("#Totalpages");
+let Status = document.querySelector("#books-status");
+
+
+let SubmitBook = document.querySelector('#submitbook');
+SubmitBook.addEventListener(onclick, () => {
+    console.log(BookTitle.value, " ", Author.value, " ", TotalPages.value, " ", Status.value);
+})
+
+// function ShowDetails() {
+//     alert(BookTitle.value, " ", Author.value, " ", TotalPages.value, " ", Status.value);
+// }
+
+// function Show() {
+//     console.log("Helo");
+// }
